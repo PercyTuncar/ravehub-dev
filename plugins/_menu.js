26349e18bf385c1f41a75e0e4ef07e56742109e4
Percy Tuncar <33427262+PercyTuncar@users.noreply.github.com>
2024-04-10 08:08:19 -0500

    CHANGE MENU

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -86,7 +86,7 @@ bot.addCommand(
       }
     })
     const date = new Date()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ RAVEHUB ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${PREFIX}
 ┃❃│ User : ${message.pushName}
